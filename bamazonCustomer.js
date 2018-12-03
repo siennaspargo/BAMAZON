d1d97809e6eb847b370002396d233e01cb75ca7b
SiennaS <ssienna48@gmail.com>
2018-12-03 18:33:41 -0500

    switchcase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,6 +24,64 @@ connection.connect(function(err) {
 
 // prompt user with 2 messages :
 // What product would you like to buy?
+function chosenProduct() {
+  inquirer
+  .prompt({
+    name: "action",
+    type: "list",
+    message: "Which Sephora product would you like to buy?",
+    choices: [
+      "OLEHENRIKSEN Nurture Me Moisturizing Crème",
+      "OOLEHENRIKSEN Counter Balance™ Oil Control Hydrator",
+      "OLEHENRIKSEN Pure Truth™ Youth Activating Oil",
+      "OOLEHENRIKSEN Uplifting Transformation™ Eye Crème",
+      "URBAN DECAY Eyeshadow Primer Potion - Original",
+      "URBAN DECAY Naked2 Palette",
+      "BITE BEAUTY Sweet Treats Agave Lip Care Set",
+      "DRYBAR Triple Sec 3-in-1",
+      "DRYBAR Detox Dry Conditioner"
+    ]
+  })
+  .then(function(answer) {
+    switch(answer.action) {
+      case "OLEHENRIKSEN Nurture Me Moisturizing Crème":
+      nurtureMe();
+      break;
+
+      case "OOLEHENRIKSEN Counter Balance™ Oil Control Hydrator":
+      balanceMe();
+      break;
+
+      case "OLEHENRIKSEN Pure Truth™ Youth Activating Oil":
+      youthSerumMe();
+      break;
+
+      case "OOLEHENRIKSEN Uplifting Transformation™ Eye Crème":
+      eyeCream();
+      break;
+
+      case "URBAN DECAY Eyeshadow Primer Potion - Original":
+      saveEnhancement();
+      break;
+
+      case "URBAN DECAY Naked2 Palette":
+      enhanceMe();
+      break;
+
+      case "BITE BEAUTY Sweet Treats Agave Lip Care Set":
+      agaveScrub();
+      break;
+
+      case "DRYBAR Triple Sec 3-in-1":
+      saveDirtyHair();
+      break;
+
+      case "DRYBAR Detox Dry Conditioner":
+      smoothifyMe();
+      break;
+    }
+  });
+}
 
 // How many of this product would you like to buy?
 
